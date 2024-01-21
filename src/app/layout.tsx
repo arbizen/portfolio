0211cc336493860502542c6df4a786874d59ee2c
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-01-21 13:22:37 +0600

    feat: add geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
-
-const inter = Inter({ subsets: ['latin'] });
+import { GeistSans } from 'geist/font/sans';
 
 export default function RootLayout({
   children,
@@ -12,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <ThemeProvider
           enableSystem
           defaultTheme="light"

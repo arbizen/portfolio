d9fd211eed9ab173a2dc79e727956e4e89d3b6e4
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-03-30 20:32:50 +0600

    feat: change font to Geist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <ThemeProvider
           defaultTheme="light"
           enableSystem

cedcf678682c47b504383112d9253041d81549ef
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2024-05-07 21:13:53 +0600

    feat: added sandpack

--- src/components/markdown/Code.tsx
+++ src/components/markdown/Code.tsx
@@ -1,7 +1,38 @@
 import { codeToHtml } from 'shiki';
+import ClientSandpack from '../client-sandpack';
+import Editor from '../client-sandpack';
+
+function removeNewline(string: string) {
+  // Remove newline character from the start and end of the string
+  string = string.replace(/^\n+/, ''); // Remove newline character from the start
+  string = string.replace(/\n+$/, ''); // Remove newline character from the end
+  return string;
+}
 
 const CodeBlock: any = async ({ ...props }) => {
   const code = props.children;
+  // check if code has the string '// editor' to render the editor
+  const isEditor = code.includes('// editor');
+  if (isEditor) {
+    const splitted =
+      code.split('// file: ') ??
+      code.split('// file:') ??
+      code.split('//file :') ??
+      code.split('//file:');
+    // get all the file names and codes except the // editor
+
+    const editorFiles: any = {};
+
+    const files = splitted.map((file: string, index: number) => {
+      if (index !== 0) {
+        const [filename, ...code] = file.split('\n');
+        const data = { filename, code: code.join('\n') };
+        editorFiles[`/${filename}`] = removeNewline(code.join('\n'));
+      }
+    });
+
+    return <Editor editorFiles={editorFiles} />;
+  }
   const html = await codeToHtml(code, {
     lang: props.className?.replace(/(?:lang(?:uage)?-)/, ''),
     theme: 'github-dark', // theme
